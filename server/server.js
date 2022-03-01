9ce0b14b004d24baa81dc8ec191a8dade013705b
nmendez4 <nmendez6594@gmail.com>
2022-03-01 11:44:09 -0500

    updated server file

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const 
+const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 
